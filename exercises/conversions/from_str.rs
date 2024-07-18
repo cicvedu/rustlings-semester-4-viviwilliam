29c9e457e99cf466a96ba911f921e57f2979294c
viviwilliam <williamishihara@gmail.com>
2024-07-18 03:17:02 -0700

    commit 7

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        match s.is_empty() {
+            true => Err(ParsePersonError::Empty),
+            false => {
+                let parts: Vec<&str> = s.split(',').collect();
+                match parts.len() != 2 {
+                    true => return Err(ParsePersonError::BadLen),
+                    false => {
+                        let name = parts[0].trim();
+                        if name.is_empty() {
+                            return Err(ParsePersonError::NoName)
+                        } 
+                        let number = parts[1].parse(); 
+                        if let Err(x) = number{
+                            return Err(ParsePersonError::ParseInt(x))
+                        }
+                        Ok(Person{name:String::from(name), age:number.unwrap()})
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -68,6 +88,7 @@ mod tests {
     fn empty_input() {
         assert_eq!("".parse::<Person>(), Err(ParsePersonError::Empty));
     }
+
     #[test]
     fn good_input() {
         let p = "John,32".parse::<Person>();
